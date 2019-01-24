6af01dc39bde6dea675450159052ac49e3fc5950
Vincent Riva <treast@MacBook-Pro-de-Vincent.local>
2019-01-24 10:28:58 +0100

    Add circle ton click

--- src/app/js/core/Canvas.ts
+++ src/app/js/core/Canvas.ts
@@ -31,57 +31,89 @@ class Canvas {
   async render() {
     requestAnimationFrame(() => this.render());
     const video = Webcam.getVideo();
-    this.ctx.drawImage(video, 0, 0, window.innerWidth, (video.height / video.width) * window.innerWidth);
     let pose = null;
     if (this.net) {
       pose = await this.net.estimateSinglePose(video, 0.5, true, 16);
-    }
+      this.ctx.save();
+      this.ctx.scale(-1, 1);
+      this.ctx.translate(window.innerWidth * -1, 0);
+      this.ctx.drawImage(video, 0, 0, window.innerWidth, (video.height / video.width) * window.innerWidth);
+      this.ctx.restore();
 
-    switch (ExperienceState.getInstance().getState()) {
-      case ExperienceState.WAITING_FOR_USER:
-        this.state.innerText = "En attente de l'utilisateur";
-        if (pose) {
-          const userKeypoints = pose.keypoints.filter((item: any) => {
-            return item.score > 0.7;
-          });
-          if (userKeypoints.length > 5) {
-            ExperienceState.getInstance().setState(ExperienceState.DRONE_IS_TAKING_OFF);
-            this.state.innerText = 'Décollage du drone';
+      switch (ExperienceState.getInstance().getState()) {
+        case ExperienceState.WAITING_FOR_USER:
+          this.state.innerText = "En attente de l'utilisateur";
+          if (pose) {
+            const userKeypoints = pose.keypoints.filter((item: any) => {
+              return item.score > 0.7;
+            });
+            if (userKeypoints.length > 5) {
+              ExperienceState.getInstance().setState(ExperienceState.DRONE_IS_TAKING_OFF);
+              this.state.innerText = 'Décollage du drone';
+            }
           }
-        }
-        break;
-      case ExperienceState.DRONE_IS_TAKING_OFF:
-        if (!this.timeout) {
-          this.timeout = setTimeout(() => {
-            ExperienceState.getInstance().setState(ExperienceState.WAITING_FOR_INTERACTION);
-            this.state.innerText = "En attente d'interaction";
-            this.timeout = null;
-          }, 4000);
-        }
-        break;
-      case ExperienceState.WAITING_FOR_INTERACTION:
-        if (pose && this.checkPose(pose)) {
+          break;
+        case ExperienceState.DRONE_IS_TAKING_OFF:
           if (!this.timeout) {
-            ExperienceState.getInstance().setState(ExperienceState.DRONE_IS_LANDING);
-            this.state.innerText = 'Atterrissage du drone';
             this.timeout = setTimeout(() => {
-              ExperienceState.getInstance().setState(ExperienceState.END_EXPERIENCE);
-              this.state.innerText = "Fin de l'expérience";
+              ExperienceState.getInstance().setState(ExperienceState.WAITING_FOR_INTERACTION);
+              this.state.innerText = "En attente d'interaction";
+              this.timeout = null;
             }, 4000);
           }
-        }
-        break;
+          break;
+        case ExperienceState.WAITING_FOR_INTERACTION:
+          const buttonX = window.innerWidth - 130;
+          const buttonY = 130;
+          const radius = 50;
+          const boundingOffset = 50;
+          const boundingBox = {
+            minX: buttonX - radius / 2 - boundingOffset,
+            minY: buttonY - radius / 2 - boundingOffset,
+            maxX: buttonX + radius / 2 + boundingOffset,
+            maxY: buttonY + radius / 2 + boundingOffset,
+          };
+
+          this.ctx.fillStyle = 'red';
+          this.ctx.arc(buttonX, buttonY, radius, 0, Math.PI * 2, true);
+          this.ctx.fill();
+
+          if (pose && this.checkPose(pose, boundingBox, { width: video.width, height: video.height })) {
+            if (!this.timeout) {
+              ExperienceState.getInstance().setState(ExperienceState.DRONE_IS_LANDING);
+              this.state.innerText = 'Atterrissage du drone';
+              this.timeout = setTimeout(() => {
+                ExperienceState.getInstance().setState(ExperienceState.END_EXPERIENCE);
+                this.state.innerText = "Fin de l'expérience";
+              }, 4000);
+            }
+          }
+          break;
+      }
     }
   }
 
-  checkPose(pose: any): boolean {
+  checkPose(pose: any, boundingBox: any, videoSize: any): boolean {
     const keypoints = pose.keypoints;
-    const leftEye = this.getKeypoint(pose, 'leftEye');
-    const rightWrist = this.getKeypoint(pose, 'rightWrist');
+    const rightWrist = this.getKeypoint(pose, 'rightWrist').position;
+    /* const leftEye = this.getKeypoint(pose, 'leftEye');
     const rightShoulder = this.getKeypoint(pose, 'rightShoulder');
     return (
       leftEye.position.y > rightWrist.position.y + this.OFFSET_MOVEMENT &&
       rightShoulder.position.x + this.OFFSET_MOVEMENT < rightWrist.position.x
+    ); */
+    const positionOnCanvas = {
+      x: (window.innerWidth / videoSize.width) * rightWrist.x,
+      y: (window.innerHeight / videoSize.height) * rightWrist.y,
+    };
+
+    console.log(positionOnCanvas);
+    console.log(boundingBox);
+    return (
+      positionOnCanvas.x >= boundingBox.minX &&
+      positionOnCanvas.x <= boundingBox.maxX &&
+      positionOnCanvas.y >= boundingBox.minY &&
+      positionOnCanvas.y <= boundingBox.maxX
     );
   }
 
